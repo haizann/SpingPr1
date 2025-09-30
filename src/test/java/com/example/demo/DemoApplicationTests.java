7f5fdc79e000b36a1593be452d133a8178891863
haizann <22115053122208@sv.ute.udn.vn>
2025-09-30 21:20:00 +0700

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplicationTests {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}

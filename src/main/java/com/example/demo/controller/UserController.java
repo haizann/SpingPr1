c6712b7353f74782577ad47c32a758b9268dc63b
haizann <22115053122208@sv.ute.udn.vn>
2025-10-01 22:43:30 +0700

    Update code

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "eric.html";
+    }
+
+}
+
+// @RestController
+// public class UserController {
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
